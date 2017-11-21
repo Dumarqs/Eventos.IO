e1b0a72038af115de8056e193be1d30bc2626276
dumarqs <eduardo_marques_@hotmail.com>
2017-11-21 11:17:18 -0200

    Commit

---
+++ Eventos.IO/ConsoleTesting/FakeUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Eventos.IO.Domain.Core.Commands;
+using Eventos.IO.Domain.Interfaces;
+
+public class FakeUnitOfWork : IUnitOfWork
+{
+    public CommandResponse Commit()
+    {
+        return new CommandResponse(true);
+    }
+
+    public void Dispose()
+    {
+        //
+    }
+}
